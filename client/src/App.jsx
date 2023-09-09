3069419f286202cf7e01496f7d83df323c53472b
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-09-09 13:12:49 +0530

    created a signup page frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Signup from './Components/Signup/Signup'
+
+function App() {
+    
+
+  return (
+    <>
+    <Signup />
+    </>
+  )
+}
+
+export default App
