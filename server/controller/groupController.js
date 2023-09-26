2d2d58fb69c9cffb7a6e1e4108ec0a521c3da52f
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-09-26 14:34:01 +0530

    superpowers of the admin

--- server/controller/groupController.js
+++ server/controller/groupController.js
@@ -87,12 +87,197 @@ const fetchUserGroups = async (req,res) => {
 }
 
 
+const fetchUsersToAdd = async (req, res) => {
+    const { groupId } = req.params; 
   
+    try {
+      // Fetch all users who are not part of the current group
+      const usersNotInGroup = await User.findAll({
+        where: {
+          id: {
+            [Op.notIn]: sequelize.literal(`
+              (SELECT UserId 
+                FROM UserGroups 
+                WHERE GroupId = ${groupId}
+              )`),
+          },
+        },
+      });
+  
+      res.status(200).json(usersNotInGroup);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+  
+const fetchUsersToRemove = async(req,res) => {
+    const { groupId } = req.params; 
+
+  try {
+    // Fetch all users who are part of the current group
+    const usersInGroup = await User.findAll({
+      where: {
+        id: {
+          [Op.in]: sequelize.literal(`
+            (SELECT UserId 
+              FROM UserGroups 
+              WHERE GroupId = ${groupId}
+            )`),
+        },
+      },
+    });
+
+    res.status(200).json(usersInGroup);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+const addToGroup = async(req,res) => {
+    try {
+        const {groupId} = req.params
+        const members = req.body.members
+
+        const group = await Group.findOne({
+            where : {
+                id: groupId
+
+                
+            }
+        })
+        if(group) {
+            const admin = await UserGroup.findOne({
+                where: {
+                    [Op.and] : [{isadmin: 1},{groupId:groupId}],
+                },
+            })
+            if(admin.userId === req.userId) {
+                const invitedMembers = await sequelize.query("SELECT * FROM Users WHERE email IN (:emails)", 
+                {
+                  replacements: { emails: members },
+                  type: QueryTypes.SELECT 
+                });
+
+                await Promise.all(
+                    invitedMembers.map(async (user) => {
+                       await UserGroup.create({
+                            isadmin: false,
+                            userId: user.id,
+                            groupId: group.id
+                        })
+                    })
+                    )
+                    res.status(201).json({message: "members added successfully"})
+            }else {
+                res.status(401).json({message: 'only admins can add members'})
+            }
+        }
+            
+        else {
+            res.status(404).json({message: "group doesnt exists"})
+        }
+    } catch (err) {
+        console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+
+const removeFromGroup = async(req,res) => {
+  try {
+    const {groupId} = req.params
+        const members = req.body.members
+
+        const group = await Group.findOne({
+            where : {
+                id: groupId
+
+                
+            }
+        })
+        if(group) {
+            const admin = await UserGroup.findOne({
+                where: {
+                    [Op.and] : [{isadmin: 1},{groupId:groupId}],
+                },
+            })
+            if(admin.userId === req.userId) {
+                const invitedMembers = await sequelize.query("SELECT * FROM Users WHERE email IN (:emails)", 
+                {
+                  replacements: { emails: members },
+                  type: QueryTypes.SELECT 
+                });
+
+                await Promise.all(
+                    invitedMembers.map(async (user) => {
+                       await UserGroup.destroy({
+                        where :{
+                          [Op.and] :[
+                            {
+                              isadmin: false,
+                            userId: user.id,
+                            groupId: group.id
+                            },
+                          ],
+                        }
+                       
+                           
+                        })
+                    })
+                    )
+                    res.status(201).json({message: "members removed successfully"})
+            }else {
+                res.status(401).json({message: 'only the admin can remove members'})
+            }
+        }
+            
+        else {
+            res.status(404).json({message: "group doesnt exists"})
+        }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+const viewMembers = async(req,res) => {
+  
+
+  try {
+    const {groupId} = req.params
+    const group = await Group.findOne({ where: { name: groupId } });
+    const userGroup = await UserGroup.findAll({
+      where: { groupId: groupId },
+    });
+
+    const users = [];
+
+    await Promise.all(
+      userGroup.map(async (user) => {
+        const res = await User.findOne({
+          where: { id: user.userId },
+        });
+        users.push(res);
+      })
+    );
+    res.status(200).json({ users: users ,userGroup});
+    // res.status(200).json( users ,userGroup);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
 
 module.exports = {
     createGroup,
     fetchUserGroups,
-    
+    fetchUsersToAdd,
+    fetchUsersToRemove,
+    addToGroup,
+    removeFromGroup,
+    viewMembers
 }
 
 
--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 const groupController = require('../controller/groupController')
 const auth = require('../middleware/auth')
 
-
+router.delete('/group/remove-from-group/:groupId',auth,groupController.removeFromGroup)
 router.post('/group/create-group',auth,groupController.createGroup)
-
+router.post('/group/add-to-group/:groupId', auth,groupController.addToGroup)
 router.get('/group/fetch-groups',auth,groupController.fetchUserGroups)
-
+router.get('/group/fetch-users-to-add/:groupId',auth,groupController.fetchUsersToAdd)
+router.get('/group/fetch-users-to-remove/:groupId',auth,groupController.fetchUsersToRemove)
+router.get('/group/view-members/:groupId',auth,groupController.viewMembers)
 
 module.exports = router
