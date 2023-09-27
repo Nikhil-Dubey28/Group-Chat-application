a3d50fc9c27a56b896ed9470f5cc8743067e2d96
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-09-27 17:44:07 +0530

    socket.io

--- client/src/Components/ChatArea/ChatArea.jsx
+++ client/src/Components/ChatArea/ChatArea.jsx
@@ -6,57 +6,91 @@ import MessageOthers from '../MessageOthers/MessageOthers';
 import MessageSelf from '../MessageSelf/MessageSelf';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios'
+import socketIOClient from 'socket.io-client';
+
+import { io } from 'socket.io-client';
+// import User from '../../../../server/model/User';
+
+const ENDPOINT = 'http://localhost:3000';
+var socket
 
 const ChatArea = () => {
   const dyParams = useParams()
   const [chat_id, chat_user] = dyParams.id.split('&')
   const [messages, setMessages] = useState([])
   const [messageContent, setMessageContent] = useState('')
-  const navigate = useNavigate()
+  const [socketConnected, setSocketConnected] = useState(false);
+  const navigate = useNavigate() 
+
+
 
   const user = JSON.parse(localStorage.getItem('user'))
+
+ useEffect(() => {
+ 
+socket = io(ENDPOINT);
+socket.emit("setup",user.id)
+socket.on("connection",() => {
+  setSocketConnected(true)
+})
+ },[])
+
+ useEffect(() => {
+  socket.on("message received", (newMessageReceived) => {
+    setMessages([...messages,newMessageReceived])
+  })
+ })
+
+  
+
   useEffect(() => {
     const fetchMessages = async (chatId) => {
       try {
         const token = localStorage.getItem('token')
-        const response = await axios.get(`http://localhost:3000/api/chat/get-message/${chatId}`,{
+        const response = await axios.get(`http://localhost:3000/api/chat/get-message/${chatId}`, {
           headers: {
-            Authorization:token
+            Authorization: token
           }
         })
         console.log(response)
         // console.log(messages)
         setMessages(response.data)
+        socket.emit("join chat", chat_id)
         console.log(messages)
       } catch (error) {
         console.log(error)
       }
     }
-    const intervalId = setInterval(() => {
-      fetchMessages(chat_id)
-    },1000)
-
-    return () => clearInterval(intervalId)
+   
     fetchMessages(chat_id)
-  },[])
+
+
+  }, [])
 
   const handleSendMessage = async () => {
     try {
       const token = localStorage.getItem('token')
 
-        const response = await axios.post(`http://localhost:3000/api/chat/send-message`,{
-          message: messageContent,
-          chatId: chat_id
-        }, {
-          headers:{
-Authorization:token
-          }
-        })
-        console.log(response)
-        setMessages([...messages,response.data.chat])
-        setMessageContent('')
+      const response = await axios.post(`http://localhost:3000/api/chat/send-message`, {
+        message: messageContent,
+        chatId: chat_id
+      }, {
+        headers: {
+          Authorization: token
+        }
+      })
+      console.log(response)
+
+      socket.emit("new message", {
+        message: messageContent,
+        chatId: chat_id,
+        name: user.name
+      })
+      setMessages([...messages, response.data.chat])
+       
+      setMessageContent('')
     } catch (error) {
-        console.log(error)
+      console.log(error)
     }
   }
   return (
@@ -78,22 +112,22 @@ Authorization:token
 
       </div>
       <div className='messages-container'>
-    {messages.map((message,index) => {
-      const senderId = message.userId 
-      const myId = user.id
-      console.log(message)
-      if(senderId == myId) {
-        return (
-          <MessageSelf message= {message.message} key={index}/>
-            
-          )
-        }else {
-          return (
-            <MessageOthers name={message.name} message={message.message} key={index} />
+        {messages.map((message, index) => {
+          const senderId = message.userId
+          const myId = user.id
+          console.log(message)
+          if (senderId == myId) {
+            return (
+              <MessageSelf message={message.message} key={index} />
 
-          )
-        }
-    })}
+            )
+          } else {
+            return (
+              <MessageOthers name={message.name} message={message.message} key={index} />
+
+            )
+          }
+        })}
         {/* <MessageOthers /> */}
         {/* <MessageSelf /> */}
         {/* <MessageOthers /> */}
@@ -103,7 +137,7 @@ Authorization:token
         <textarea type="text" placeholder='Type a message' className='search-box'
           value={messageContent}
           onChange={(e) => setMessageContent(e.target.value)}
-          style={{width: '100%'}}
+          style={{ width: '100%' }}
         />
         <IconButton
           onClick={handleSendMessage}
--- server/server.js
+++ server/server.js
@@ -5,10 +5,19 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const sequelize = require('./database/configDatabase')
 const userRoutes = require('./routes/userRoutes')
+const chatRoutes = require('./routes/chatRoutes')
+const groupRoutes = require ('./routes/groupRoutes')
+const Chat = require('./model/Chat')
+const User = require('./model/User')
+const Group = require('./model/Group')
+const UserGroup = require('./model/UserGroup')
 
 
 // middlewares
-app.use(cors())
+
+app.use(cors({
+    origin:"*"
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -34,13 +43,53 @@ Group.hasMany(UserGroup);
 UserGroup.belongsTo(User);
 UserGroup.belongsTo(Group);
 
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is runnning on port ${PORT}`)
+})
+
+const io = require('socket.io')(server,{
+    pingTimeout : 60000,
+    cors: {
+        origin: "*",
+    }
+});
+
+io.on("connection",(socket) => {
+console.log('connected to socket.io');
+
+socket.on('setup', (userData) => {
+socket.join(userData)
+console.log(userData)
+socket.emit("connected")
+})
+
+socket.on("join chat",(room) => {
+socket.join(room)
+console.log("User joined room: " + room)
+})
+
+socket.on("new message", (newMessageReceived) => {
+    const  {message,chatId,name} = newMessageReceived;
+
+    socket.broadcast.to(chatId).emit("message received", {
+        message,
+        name
+    })
+})
+
+socket.off("setup",() => {
+    conosole.log('USER DISCONNECTED')
+    socket.leave(userData)
+})
+})
+
+
 sequelize
 .sync()
 .then(() => {
     console.log('connected to database')
-    app.listen(process.env.PORT,() => {
-        console.log(`Server is running on http://localhost:${process.env.PORT}`)
-    })
+   
 })
-
 
